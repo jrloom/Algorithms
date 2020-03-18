5687c4ef6b077b8529a294bb7c82671caabc00d1
Jason Loomis <jason@jrloomis.com>
2020-03-18 17:01:43 -0400

    recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,34 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # track batches - start with an empty list that we'll add stuff to bsaed on the criteria below
+    batches = []
+
+    for i in recipe:
+
+        # check ingredients
+        if i not in ingredients:
+            # print("nope")
+            return 0
+
+        # if (on-hand ingredients) / (amount needed) is >= 1
+        elif ingredients[i] // recipe[i] >= 1:
+            # add that to the batches list
+            batches.append(ingredients[i] // recipe[i])
+            # print(f"bacthes: {batches}")
+
+    return min(batches)
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+if __name__ == "__main__":
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {"milk": 100, "butter": 50, "flour": 5}
+    ingredients = {"milk": 132, "butter": 48, "flour": 51}
+    print(
+        "{batches} batches can be made from the available ingredients: {ingredients}.".format(
+            batches=recipe_batches(recipe, ingredients), ingredients=ingredients
+        )
+    )
