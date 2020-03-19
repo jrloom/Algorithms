395914b8ee16f45ccf38acc6872690409a02ef62
Jason Loomis <jason@jrloomis.com>
2020-03-19 16:20:43 -0400

    rpck paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,35 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+
+    # define possible moves
+    moves = ["rock", "paper", "scissors"]
+
+    # the list we'll populate, and return
+    possible = []
+
+    def rps_recursive(num, list=[]):
+        # base case
+        if num == 0:
+            return possible.append(list)
+
+        else:
+            # loop through moves, add to list, repeat until num == 0
+            for i in range(len(moves)):
+                rps_recursive(num - 1, list + [moves[i]])
+
+        return [possible]
+
+    rps_recursive(n)
+
+    return possible
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print("Usage: rps.py [num_plays]")
