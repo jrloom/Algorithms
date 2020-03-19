80bc72efbeaeb834bfe78d0fef752548df4791b4
Jason Loomis <jason@jrloomis.com>
2020-03-19 15:52:48 -0400

    eating cookies - different recursion method

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python
 
+# 0:  0 cookie * 1 time
+# 1:  1 cookie * 1 time
+# 2:  1 cookie * 2 times  ||  2 cookies * 1 time
+# 3:  1 cookie * 3 times  ||  2 cookies * 1 time + 1 cookie * 1 time  ||  1 cookie * 1 time + 2 cookies * 1 time  ||  3 cookies * 1 time
+
+
 import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache={}):
+# def eating_cookies(n, cache={}):
 
-    # base cases
+# base cases
+
+# if n < 0:
+#     return 0
+
+# elif n == 0:
+#     return 1
+
+# elif n not in cache:
+#     cache[n] = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+# return cache[n]
+
+
+def eating_cookies(n, cache=None):
 
     if n < 0:
         return 0
@@ -15,10 +35,22 @@ def eating_cookies(n, cache={}):
     elif n == 0:
         return 1
 
-    elif n not in cache:
-        cache[n] = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    elif cache and cache[n] > 0:
+        return cache[n]
 
-    return cache[n]
+    else:
+        if cache is None:
+            cache = {}
+
+        value = (
+            eating_cookies(n - 1, cache)
+            + eating_cookies(n - 2, cache)
+            + eating_cookies(n - 3, cache)
+        )
+
+        cache[n] = value
+
+        return value
 
 
 if __name__ == "__main__":
