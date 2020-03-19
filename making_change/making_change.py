58a6c5366295e1e3a24cbf543f3782e31c9f27c8
Jason Loomis <jason@jrloomis.com>
2020-03-19 16:52:45 -0400

    making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,40 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+
+    # initialize cache
+    possible = [0] * (amount + 1)
+    # print(f"initial cache: {possible}")
+
+    # 0 cents
+    possible[0] = 1
+
+    # first loop returns possibilities
+    for val in denominations:
+        # loop through higher amounts between val and amount
+        for higher_amount in range(1, amount + 1):
+            if val <= higher_amount:
+                # if denom <= current, add to possible
+                possible[higher_amount] += possible[higher_amount - val]
+                # print(f"val {val} | ha {higher_amount}")
+                # print(f"cache: {possible}")
+                # print("\n--------------------\n")
+
+    return possible[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print(
+            "There are {ways} ways to make {amount} cents.".format(
+                ways=making_change(amount, denominations), amount=amount
+            )
+        )
+    else:
+        print("Usage: making_change.py [amount]")
